ae3c19ebf3748f88a7a97152c8127056350736c4
Will Scripps <WLScripps@gmail.com>
2017-11-24 16:44:57 -0500

    CRUD for schedules

--- app/controllers/api/v1/schedules_controller.rb
+++ app/controllers/api/v1/schedules_controller.rb
@@ -21,5 +21,22 @@ class Api::V1::SchedulesController < ApplicationController
     end
   end
 
+  def update
+    schedule_info = params["schedule"]
+    schedule = Schedule.find(schedule_info['id'])
+
+    if schedule.update(scheduled_start: DateTime.parse(schedule_info['scheduled_start']), scheduled_end: DateTime.parse(schedule_info['scheduled_end']), description: schedule_info['description'])
+      render json: schedule
+    else
+      render json: {errors: schedule.errors.full_messages}, status: 422
+    end
+  end
+
+
+  def destroy
+    Schedule.destroy(params['schedule']['id'])
+    render json: Schedule.all
+  end
+
 
 end
