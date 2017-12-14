23a6eb0186b8414ce64a9be2cc436dc78e7a3e04
Will Scripps <WLScripps@gmail.com>
2017-12-14 12:24:37 -0500

    added origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,6 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localhost:3001'
+    origins 'carpentry-manager.herokuapp.com'
 
     resource '*',
       headers: :any,
